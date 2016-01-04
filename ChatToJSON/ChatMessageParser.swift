fa968f1b95486e53e7e1a2d18709538cec618c42
Bjorn Ho <juwen.ho@gmail.com>
2016-01-04 23:37:29 +1100

    Add a chat message parser
    A class to parse the chat message for special content. Internally keeps a list of objects that conform to a JSON content extractable protocol which can build the special content JSON incrementally.
    
    This is so that if new content extractors are added, it can be added with ease. Also, if the existing content extractors need to be changed internally, this class does not need to know about those changes

---
+++ ChatToJSON/ChatMessageParser.swift
@@ -0,0 +1,56 @@
+//
+//  ChatMessageParser.swift
+//  ChatToJSON
+//
+//  Created by Bjorn Ho on 4/01/2016.
+//  Copyright © 2016 Bjorn Ho. All rights reserved.
+//
+
+import Foundation
+
+protocol JSONContentExtractable {
+    var key: String { get }
+    
+    func extractJSONContent(string: String) -> [AnyObject]?
+}
+
+class ChatMessageParser {
+    var contentExtractors: [JSONContentExtractable] = [JSONContentExtractable]()
+    
+    init() {
+        contentExtractors.append(MentionExtractor())
+    }
+    
+    func parseContent(message: String) -> String? {
+        let json = buildJSONDictionary(message)
+        
+        do {
+            let jsonData = try NSJSONSerialization.dataWithJSONObject(json, options: .PrettyPrinted)
+            return NSString(data: jsonData, encoding: NSUTF8StringEncoding) as? String
+        }
+        catch {
+            fatalError("Error: \(error)")
+        }
+        
+        return nil
+    }
+    
+    func buildJSONDictionary(message: String) -> [String : AnyObject] {
+        var json = [String : AnyObject]()
+        for contentExtractor in contentExtractors {
+            if let content = contentExtractor.extractJSONContent(message) {
+                json[contentExtractor.key] = content
+            }
+        }
+        return json
+    }
+}
+
+extension String {
+    var nsrange: NSRange { get { return NSMakeRange(0, characters.count) } }
+    
+    func substringFromNSRange(range: NSRange) -> String {
+        let a  = Range(start: startIndex.advancedBy(range.location), end: startIndex.advancedBy(range.location + range.length))
+        return substringWithRange(a)
+    }
+}
